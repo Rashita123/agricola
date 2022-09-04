f0850074def873dc2c1434a1653e162500d5e5d8
Rashita <rrashita_be19@thapar.edu>
2022-09-04 12:03:28 +0530

    feat: Admin private routing

--- src/App.js
+++ src/App.js
@@ -8,38 +8,15 @@ import {
   Borrower,
   Landing
 } from "./pages";
-import { Admin, AdminLogin } from "./admin";
+import { Admin, AdminLogin, AdminPrivateRoute } from "./admin";
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/global";
 import { USER_PROFILE } from "./utilities/constants";
 import { PrivateRoute } from "./PrivateRoute";
-import { UseAuthenticationContext } from "./context/AuthenticationContext";
 function App() {
-  const { userState } = UseAuthenticationContext();
   return (
     <div className="App">
       <Navbar />
-      {/* <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/lend" element={<Lender />} />
-        <Route path="/stake" element={<Staker />} />
-        <Route path="/borrow" element={<Borrower />} />
-        {userState.loginAsAdmin && <Route path="/admin" element={<Admin/>}/> }
-        <Route path="/adminlogin" element={<AdminLogin/>}/> */}
-      {/* <PrivateRoute path="/admin"
-          element={<Admin />}/> */}
-      {/* <Route
-          path={`/${USER_PROFILE.PROFILE_PATH}`}
-          element={<ProfilePage />}
-        />
-        <Route path={`/${USER_PROFILE.KYC_PATH}`} element={<ProfilePage />} />
-      </Routes> */}
-      {/* <Route path="/" element={<Home/>}/> */}
-      {/* <Route path="/" element={<PrivateRoute />}>
-        <Route element={< Home />}/>
-      </Route> */}
-
       <Routes>
       <Route path="/landing" element={<Landing />} />
         <Route path="/" element={<PrivateRoute />}>
@@ -51,6 +28,9 @@ function App() {
             element={<ProfilePage />}
           />
           <Route path={`/${USER_PROFILE.KYC_PATH}`} element={<ProfilePage />} />
+        </Route>
+
+        <Route path="/" element={<AdminPrivateRoute />}>
           <Route path="/admin" element={<Admin />} />
         </Route>
 
